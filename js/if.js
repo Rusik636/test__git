badbd245367b34733b124694eecf839bdcd6e734
Rusik636 <ruslon.poksin.52@gmail.com>
2021-01-20 19:18:53 +0300

    add js script

---
+++ js/if.js
@@ -0,0 +1,14 @@
+
+// let number = 10;
+
+// while (number < 100) {
+//     console.log(number);
+//     number++;
+// }
+
+for (let i = 1; i <= 8; i++) {
+    if (i == 6) {
+        break
+    } 
+    console.log(i);
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-let row = 5;
+let money = prompt("Ваш бюджет в месяц",''),
+    time  = prompt("Введите дату в формате YYYY-MM-DD",'');
 
-console.log (row);
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+let a1 = prompt("Введите обязательную статью расходов?",''),
+    a2 = prompt("Восколько обойдется?",''),
+    a3 = prompt("Введите обязательную статью расходов?",'')
+    a4 = prompt("Восколько обойдется?",'');
 
-let fer = 7;
+ appData.expenses.a1 = a2;
+ appData.expenses.a3 = a4;
+
+ alert(appData.budget / 30);
